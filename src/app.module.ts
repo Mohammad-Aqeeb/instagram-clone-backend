f6ca311f1f5a6fd91a44e6bbc71d58e3e02b32c5
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-10 21:58:57 +0530

    change in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { PostsModule } from './modules/posts/posts.module';
+import { FilesModule } from './modules/files/files.module';
 
 @Module({
-  imports: [UserModule, PostsModule],
+  imports: [UserModule, PostsModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
