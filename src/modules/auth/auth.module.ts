2b32f9f2a417bca49b1252dd4f372e2497b78995
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-11 22:28:31 +0530

    change in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { userEntity } from '../user/entity/user.entity';
+import { UserService } from '../user/user.service';
 
 @Module({
-  imports : [
-    
-  ],
-  providers: [AuthService],
+  imports : [TypeOrmModule.forFeature([userEntity])],
+  providers: [AuthService, UserService],
   controllers: [AuthController]
 })
 export class AuthModule {}
