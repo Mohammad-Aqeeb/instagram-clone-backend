15175fbbf3a67d486817b456d6373834a8808ee9
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-11 21:40:23 +0530

    change in user entity

---
+++ src/modules/user/entity/user.entity.ts
@@ -0,0 +1,74 @@
+import { Exclude } from "class-transformer";
+import { IsEmail } from "class-validator";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class userEntity{
+
+    @Column({length : 64})
+    name : string
+
+    @Column({unique : true, length : 24})
+    username : string
+
+    @Column({unique : true})
+    @IsEmail()
+    email : string
+
+    @Exclude()
+    @Column({nullable : true, length : 128})
+    password : string
+
+
+    @Exclude()
+    @Column({nullable : true, select : false})
+    hashedRefreshtoken : string
+
+
+    @Column({default : false})
+    isTwoFactorEnable : boolean
+
+    @Exclude()
+    @Column({nullable : true, select : false})
+    twoFactorSecret : string
+
+    @Column({nullable : true})
+    locale : string
+
+    @Column({default : true})
+    isActive : boolean
+
+    @Column({default: false})
+    isEmailConfirmed : boolean
+
+    @Column({default : false})
+    isOAuthAccount: boolean;
+    
+    @Exclude()
+    @Column({ default : false})
+    isGoogleAccount : boolean
+
+    @Exclude()
+    @Column({default : false})
+    isGithubAccount : boolean
+
+    @Column({default : '#b3e6ff'})
+    color : string
+
+
+
+
+    @Column({ length: 1024, nullable: true })
+    description: string;
+    
+    @Column({ length: 512, nullable: true })
+    website: string;
+    
+    @Column({ length: 64, nullable: true })
+    phone: string;
+    
+    @Column({ length: 64, nullable: true })
+    gender: string;
+
+    
+}
