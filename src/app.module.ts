a7b9fec260013b576dfb7b1c65313f2fd683d934
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-12 18:00:54 +0530

    change in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,21 @@ import { PostsModule } from './modules/posts/posts.module';
 import { FilesModule } from './modules/files/files.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PostsModule, FilesModule, NotificationsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      url : process.env.DATABASE_URL,
+      autoLoadEntities : true,
+      synchronize :true
+    }),
+    UserModule, PostsModule, FilesModule, NotificationsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
