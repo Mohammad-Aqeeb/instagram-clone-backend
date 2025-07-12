9b8208292fce573f7906ca25aca81b61bf2c0896
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-12 17:57:47 +0530

    change in auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
 import { UserService } from '../user/user.service';
-import { userEntity } from '../user/entity/user.entity';
+import { UserEntity } from '../user/entity/user.entity';
+import { CreateUserDTO } from '../user/dto/user.dto';
 
 @Controller('auth')
 export class AuthController {
 
-    
     constructor(private readonly userService : UserService){}
 
     @Post('register')
-    async createUser(@Body() payload : Partial<userEntity>) : Promise<userEntity>{
+    async createUser(@Body() payload : CreateUserDTO) : Promise<UserEntity>{
         return await this.userService.create(payload);
+    }
+
+    @Get()
+    async getAll() : Promise<UserEntity[]>{
+        return this.getAll();
     }
 }
