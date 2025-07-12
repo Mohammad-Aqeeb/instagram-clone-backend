28234cb5118bb371f8d952e5d1a0f85584357d42
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-12 17:59:09 +0530

    change in user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { UserEntity } from './entity/user.entity';
+import { CreateUserDTO } from './dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectRepository(UserEntity) private userRepository : Repository<UserEntity>){}
+
+    async create (payload : CreateUserDTO): Promise<UserEntity>{
+        const existingUser = await this.userRepository.findOne({where : {email : payload.email}});
+        if(existingUser){
+            throw new HttpException("User already exists", HttpStatus.BAD_REQUEST);
+        }
+        
+        const user = this.userRepository.create(payload);
+        return await this.userRepository.save(user);
+    }
+}
