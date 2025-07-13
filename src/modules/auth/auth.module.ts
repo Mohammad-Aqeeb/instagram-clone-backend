d520014e6c1795037fbb62843f2c53194e7dad3c
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-13 23:30:09 +0530

    change in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UserService } from '../user/user.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 
 @Module({
   imports : [
     UserModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN},
+    JwtModule.registerAsync({
+        useFactory: () => ({
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+        }),
     }),
 
   ],
