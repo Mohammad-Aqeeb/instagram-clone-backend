f806fce87984e5547ae15bf0088a94a2fc0677b4
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-15 17:35:54 +0530

    change in auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt'
 
-import { CreateUserDTO, UserJwtPayload, UserTokensInterface } from '../user/dto/user.dto';
+import { CreateUserDTO, UserJwtPayload, UserTokensInterface, UserValidationDTO } from '../user/dto/user.dto';
 import { UserEntity } from '../user/entity/user.entity';
 import { UserService } from '../user/user.service';
 
@@ -14,8 +14,21 @@ export class AuthService {
         private readonly jwtService : JwtService
     ){}
 
+    async validateUser(data : UserValidationDTO) : Promise<UserEntity>{
+        const user = await this.userService.getUserByEmail(data.email)
+        if(!user){
+            throw new HttpException("USER_NOT_FOUND", HttpStatus.UNAUTHORIZED)
+        }
+
+        const isPasswordMatch = await user.validatePassword(data.password)
+        if(isPasswordMatch){
+            return user;
+        }
+        else throw new HttpException('USER_INVALID_CREDENTIALS', HttpStatus.UNAUTHORIZED);
+    }
+
     async register(payload : CreateUserDTO) : Promise<UserEntity>{
-        const user = this.userService.create(payload);
+        const user = await this.userService.create(payload);
         return user;
     }
 
@@ -30,13 +43,13 @@ export class AuthService {
         }
         const payload : UserJwtPayload = {id : user.id , email : user.email, is2FAEnabled : is2FAEnabled}
 
-        const accessToken = this.jwtService.sign(payload);
+        const accessToken = this.jwtService.sign(payload,{secret: process.env.JWT_SECRET});
         const refreshToken =  this.jwtService.sign(payload,{
             expiresIn : process.env.JWT_REFRESH_EXPIRES_IN
         });
 
         const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
-        await this.userService.setUpdateRefreshToken(user.id, hashedRefreshToken)
+        await this.userService.setUpdateRefreshToken(dbUser.id, hashedRefreshToken)
 
         return {
             user,
