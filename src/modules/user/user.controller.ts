35778315393194b5e6189b6462d5af8ca0c1c4f2
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-15 18:10:30 +0530

    change in user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, Request, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserEntity } from './entity/user.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
+import { Public } from '../auth/decorator/public.decorator';
 
 @Controller('user')
 export class UserController {
-    
+  
+    constructor(private userService : UserService){}
+
+    // @Get()
+    //     async getAll(@Query('search') search: string, @Request() req): Promise<UserEntity[]> {
+    //     return await this.userService.getAll(search, req.user.id);
+    // }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':username')
+    async getProfileByUsername(@Param('username') username: string, @Request() req): Promise<UserEntity> {
+        return await this.userService.getProfileByUsername(username, req.user.id);
+    }
 }
