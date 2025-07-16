4345e764865b538151ba07510935b7a1a134d9d8
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-16 19:53:38 +0530

    change in files entity

--- src/modules/files/files.module.ts
+++ src/modules/files/files.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileEntity } from './entity/file.entity';
 
 @Module({
-  providers: [FilesService]
+  imports : [TypeOrmModule.forFeature([FileEntity])],
+  providers: [FilesService],
+  exports : [FilesService]
 })
 export class FilesModule {}
