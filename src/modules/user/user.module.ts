731f40f563ae58a54f03b061719d269145422a0d
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-17 22:08:46 +0530

    change in user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
+import { FollowingEntity } from './entity/following.entity';
+import { RecentSearchEntity } from './entity/recentSearch.entity';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([UserEntity])],
+  imports : [TypeOrmModule.forFeature([UserEntity, FollowingEntity, RecentSearchEntity])],
   controllers: [UserController],
   providers: [UserService],
   exports : [UserService]
