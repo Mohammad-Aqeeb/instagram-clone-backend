855027ef1453394fc6d852b032b80d6733bced2d
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-17 22:02:57 +0530

    change in jwt auth guardd

--- src/modules/auth/guards/jwt-auth/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth/jwt-auth.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
@@ -15,7 +15,6 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       context.getHandler(),
       context.getClass()
     ])
-    
     if(isPublic) return true
 
     return super.canActivate(context);
