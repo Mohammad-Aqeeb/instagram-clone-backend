7ec07953dfb09619b34be52f0e05a5f169192a62
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-19 23:05:57 +0530

    change in post module

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from './entity/post.entity';
 import { PostLikeEntity } from './entity/postLike.entity';
 import { CommentEntity } from './entity/comment.entity';
+import { CommentLikeEntity } from './entity/commentLike.entity';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([PostEntity, PostLikeEntity, CommentEntity])],
+  imports : [TypeOrmModule.forFeature([PostEntity, PostLikeEntity, CommentEntity, CommentLikeEntity])],
   providers: [PostsService],
   controllers: [PostsController]
 })
