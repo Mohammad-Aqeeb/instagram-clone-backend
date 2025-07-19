75f6fb2e3b098bca0cd3f4134e3efccaa65594ec
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-19 22:24:58 +0530

    create post entity

---
+++ src/modules/posts/entity/post.entity.ts
@@ -0,0 +1,50 @@
+import { BaseEntity } from "src/common/types/base.entity";
+import { UserEntity } from "src/modules/user/entity/user.entity";
+import { AfterLoad, Column, Entity, ManyToOne, OneToMany, OneToOne } from "typeorm";
+import { PostLikeEntity } from "./postLike.entity";
+import { CommentEntity } from "./comment.entity";
+import { FileEntity } from "src/modules/files/entity/file.entity";
+
+@Entity()
+export class PostEntity extends BaseEntity{
+    @Column({nullable : true})
+    description : string
+
+
+
+    @ManyToOne(()=> UserEntity, (u)=> u.posts, {
+        cascade : true,
+        eager : true
+    })
+    user : UserEntity
+   
+    @OneToMany(()=> PostLikeEntity, (p)=>p.post, {
+        cascade : true
+    })
+    like : PostLikeEntity[];
+    likeNumber : number;
+
+    @OneToMany(()=> CommentEntity, (c)=> c.post,{
+        cascade : true
+    })
+    comment : CommentEntity
+    commentNumber : string
+
+
+
+
+    @Column({ type: 'boolean', default: false })
+    isVideo: boolean;
+
+    @OneToOne(()=> FileEntity,{
+        cascade : true
+    })
+    file : FileEntity;
+    fileUrl : string
+
+    @AfterLoad()
+    getFileUrl(){
+        this.fileUrl = this.file.url;
+    }
+
+}
