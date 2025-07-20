1cbaacee322b90906913c8a91da381bf0f55d3cd
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-20 23:08:28 +0530

    change in post controller

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostEntity } from './entity/post.entity';
+import { TagEntity } from './entity/tag.entity';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+
+    constructor(private readonly postService : PostsService){}
+
+    @Get('tags')
+    async getByTag(@Query('search') search : string) : Promise<TagEntity[]>{
+        return this.postService.getByTag(search);
+    }
+}
