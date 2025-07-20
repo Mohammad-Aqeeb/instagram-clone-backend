58d4b40f0cc57154eb88a0dc7f8290be5940563d
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-20 23:08:37 +0530

    change in post service

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -2,11 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { PostEntity } from './entity/post.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { TagEntity } from './entity/tag.entity';
 
 @Injectable()
 export class PostsService {
 
-    constructor(@InjectRepository(PostEntity) PostEntity : Repository<PostEntity>){}
+    constructor(
+        @InjectRepository(PostEntity) private postRepository : Repository<PostEntity>,
+        @InjectRepository(TagEntity) private tagRepository : Repository<TagEntity>
+    ){}
 
-    async 
+    async getByTag(search : string) : Promise<TagEntity[]>{
+        return this.tagRepository.createQueryBuilder('tag')
+            .select('tag')
+            .from(TagEntity, 'tag')
+            .leftJoin('tag.posts', 'posts')
+            .addSelect('Count(posts)', 'count')
+            .where('tag.name ILIKE search' , {search : `%${search}%`})
+            .orderBy('count', 'DESC')
+            .groupBy('tag.id')
+            .take(20)
+            .getMany();
+
+    }
 }
