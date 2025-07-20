59cae74f5a3df6a34fcfda3ed9d48b67d3e73f2d
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-20 20:51:12 +0530

    change in post module

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -6,9 +6,10 @@ import { PostEntity } from './entity/post.entity';
 import { PostLikeEntity } from './entity/postLike.entity';
 import { CommentEntity } from './entity/comment.entity';
 import { CommentLikeEntity } from './entity/commentLike.entity';
+import { ReportEntity } from './entity/report.entity';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([PostEntity, PostLikeEntity, CommentEntity, CommentLikeEntity])],
+  imports : [TypeOrmModule.forFeature([PostEntity, PostLikeEntity, CommentEntity, CommentLikeEntity, ReportEntity])],
   providers: [PostsService],
   controllers: [PostsController]
 })
