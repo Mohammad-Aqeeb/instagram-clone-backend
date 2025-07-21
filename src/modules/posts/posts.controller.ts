ef96faa1d5ae5466cc0cc0767333924b43930741
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-21 17:21:09 +0530

    change in post controller

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Request, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostEntity } from './entity/post.entity';
 import { TagEntity } from './entity/tag.entity';
+import { CommentEntity } from './entity/comment.entity';
+import { PostLikeEntity } from './entity/postLike.entity';
+import { UserEntity } from '../user/entity/user.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreatePostDTO } from './dto/post.dto';
 
 @Controller('posts')
 export class PostsController {
@@ -9,7 +14,37 @@ export class PostsController {
     constructor(private readonly postService : PostsService){}
 
     @Get('tags')
-    async getByTag(@Query('search') search : string) : Promise<TagEntity[]>{
-        return this.postService.getByTag(search);
+    async getTags(@Query('search') search : string) : Promise<TagEntity[]>{
+        return await this.postService.getTags(search);
+    }
+
+    @Get(':id')
+    async getPostById(@Param('id') id : number) : Promise<PostEntity>{
+        return await this.postService.getPostById(id)
+    }
+
+    // @Get('comments/:id')
+    // async getComments(@Param('id') id:string, @Request() req) : Promise<CommentEntity>{
+    //     return await this.postService.getComments(id)
+    // }
+
+    @Get('likes/:id')
+    async getLikes(@Param('id') id:number, @Request() req) : Promise<Partial<UserEntity>[]>{
+        return await this.postService.getLikes(id,req.user.id);
+    }
+
+    @Get('tags/:name')
+    async getTagByName(@Param('name') name : string) : Promise<TagEntity>{
+        return await this.postService.getTagByName(name);
+    }
+
+    @UseInterceptors(FileInterceptor('file'))
+    @Post()
+    async createPost(
+        @UploadedFile() file : Express.Multer.File,
+        @Body() payload : CreatePostDTO,
+        @Request() req
+    ) : Promise<PostEntity>{
+        return this.postService.createPost(file, payload, req.user.id)
     }
 }
