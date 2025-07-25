09b13f1c19a0c722265fd6cc3a7164cdba91431f
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-25 15:45:42 +0530

    change in notification module

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationEntity } from './entity/notification.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports : [
+    TypeOrmModule.forFeature([NotificationEntity]),
+    forwardRef(() => UserModule),
+  ],
+  exports: [NotificationsService],
   providers: [NotificationsService],
   controllers: [NotificationsController]
 })
