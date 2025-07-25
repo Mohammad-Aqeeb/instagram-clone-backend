7be91ce1be44c55c613b2fac32ea006b6087e24f
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-25 15:47:40 +0530

    change in user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -7,11 +7,13 @@ import { FollowingEntity } from './entity/following.entity';
 import { RecentSearchEntity } from './entity/recentSearch.entity';
 import { FilesModule } from '../files/files.module';
 import { PostsModule } from '../posts/posts.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports : [
     TypeOrmModule.forFeature([UserEntity, FollowingEntity, RecentSearchEntity]),
     FilesModule,
+    forwardRef(() => NotificationsModule),
     forwardRef(() => PostsModule)
   ],
   controllers: [UserController],
