72bf364cb6fc122a7b2c93736727ad2a846421f7
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-07-27 16:17:21 +0530

    change in main.js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config as awsConfig} from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
 
+  const configService = app.get(ConfigService)
+  awsConfig.update({
+    accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+    region: configService.get('AWS_REGION'),
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
